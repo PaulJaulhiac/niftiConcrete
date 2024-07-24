ff06466f2f9804dabef3f16e65a8563002ef0c69
PauloElFamoso <153619838+PauloElFamoso@users.noreply.github.com>
2024-07-24 11:37:51 -0400

    Update convertTiff2Jpg.sh
    1st commit

--- convertTiff2Jpg.sh
+++ convertTiff2Jpg.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Define the input and output directories
-input_dir=~/Documents/niftiConcrete/y01/
-output_dir=~/Documents/niftiConcrete/y01_nifti/
-input_dir2=~/Documents/niftiConcrete/y00/
-output_dir2=~/Documents/niftiConcrete/y00_nifti/
+input_dir=~/pathToYourInputDir
+output_dir=~/pathToYourOutputDir
+input_dir2=~/pathToYourInputDir2
+output_dir2=~/pathToYourOutputDir2
 
 # Iterate over the range of files
 for i in $(seq -f "%05g" 0 19); do
-    input_file="${input_dir}20231210_191049_Claudiane_LC3-2_11132023_comp_0_x00y01_${i}.tiff"
-    output_file="${output_dir}20231210_191049_Claudiane_LC3-2_11132023_comp_0_x00y01_${i}.jpg"
+    input_file="${input_dir}nameOfTiffFileUntilIdentifier_${i}.tiff"
+    output_file="${output_dir}nameOfJPGFileUntilIdentifier_${i}.jpg"
     
     # Convert TIFF to JPG
     convert "$input_file" "$output_file"
@@ -18,8 +18,8 @@ for i in $(seq -f "%05g" 0 19); do
     echo "Converted $input_file to $output_file"
 done
 for i in $(seq -f "%05g" 1328 1347); do
-    input_file="${input_dir2}20231210_191049_Claudiane_LC3-2_11132023_comp_0_x00y00_${i}.tiff"
-    output_file="${output_dir2}concrete_y00_${i}.jpg"
+    input_file="${input_dir2}nameOfTiffFileUntilIdentifier_${i}.tiff"
+    output_file="${output_dir2}nameOfJPGFileUntilIdentifier_${i}.jpg"
     
     # Convert TIFF to JPG
     convert "$input_file" "$output_file"
